874cacb9f22aa51697331ec2de136606bc5b9040
sangwon <swchoi1997@naver.com>
2022-05-22 14:17:02 +0900

    add: 프로젝트 생성

---
+++ src/main/java/com/example/godtudy/GoDtudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.godtudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoDtudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoDtudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/godtudy/GoDtudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.godtudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoDtudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
